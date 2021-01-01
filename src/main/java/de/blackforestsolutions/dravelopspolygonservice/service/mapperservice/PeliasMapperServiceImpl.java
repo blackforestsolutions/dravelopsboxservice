243b34f60505407f2cd6e3e4a5c9f39034cf38a4
hennihaus <jan-hendrik.hausner@outlook.com>
2021-01-01 21:19:57 +0100

    added datamodel update #patch

--- src/main/java/de/blackforestsolutions/dravelopspolygonservice/service/mapperservice/PeliasMapperServiceImpl.java
+++ src/main/java/de/blackforestsolutions/dravelopspolygonservice/service/mapperservice/PeliasMapperServiceImpl.java
@@ -1,12 +1,12 @@
 package de.blackforestsolutions.dravelopspolygonservice.service.mapperservice;
 
 import de.blackforestsolutions.dravelopsdatamodel.CallStatus;
+import de.blackforestsolutions.dravelopsdatamodel.Point;
 import de.blackforestsolutions.dravelopsdatamodel.Status;
 import de.blackforestsolutions.dravelopsdatamodel.TravelPoint;
 import de.blackforestsolutions.dravelopsgeneratedcontent.pelias.Feature;
 import de.blackforestsolutions.dravelopsgeneratedcontent.pelias.Geometry;
 import de.blackforestsolutions.dravelopsgeneratedcontent.pelias.PeliasTravelPointResponse;
-import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -43,9 +43,9 @@ public class PeliasMapperServiceImpl implements PeliasMapperService {
     }
 
     private Point extractPointFrom(Geometry geometry) {
-        return new Point(
+        return new Point.PointBuilder(
                 geometry.getCoordinates().get(FIRST_INDEX),
                 geometry.getCoordinates().get(SECOND_INDEX)
-        );
+        ).build();
     }
 }
